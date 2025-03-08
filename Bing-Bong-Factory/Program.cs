9744c18662ab5391b235a3600740c36af5da400f
Julio <Julioflorentino91@gmail.com>
2025-03-07 22:23:48 -0400

    La exoneracion huele

--- Bing-Bong-Factory/Program.cs
+++ Bing-Bong-Factory/Program.cs
@@ -16,7 +16,7 @@ namespace Bing_Bong_Factory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login_form());
         }
     }
 }
