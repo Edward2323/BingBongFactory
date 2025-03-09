03f1f42bee5431080f54071cd71c1286383b8a6b
edward cabrera <edwardcabrera2323@gmail.com>
2025-03-08 22:00:05 -0500

    Elimar carpeta extra bing bong factory

---
+++ Sign_in.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bing_Bong_Factory
+{
+    public partial class Sign_in : Form
+    {
+        public Sign_in()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_click(object sender, EventArgs e)
+        {
+
+            Login_form Log = new Login_form();
+            Log.Show();
+            this.Close();
+
+        }
+    }
+}
