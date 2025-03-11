6ac25b1b7b7c4163c6a9785229ebf6e6b8a2163f
edward cabrera <edwardcabrera2323@gmail.com>
2025-03-10 23:38:42 -0400

    Concluir inserccion y eliminacion de producto

--- Presentacion/Inventario/FrmEditar.cs
+++ Presentacion/Inventario/FrmEditar.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bing_Bong_Factory.Datos;
+using Bing_Bong_Factory.Negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +9,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Bing_Bong_Factory.Presentacion;
 
 namespace Bing_Bong_Factory.Presentacion.Inventario
 {
+    
+    
     public partial class FrmEditar : Form
     {
-        public FrmEditar()
+        Dashboard_form frm = new Dashboard_form();
+        DBconnection db = new DBconnection();
+        string id;
+        public FrmEditar(string productoid, string nombre, string precio, string cantidad)
         {
             InitializeComponent();
+
+            this.id = productoid;
+            txtNombreproductoE.Text = nombre;
+            txtPrecioE.Text = precio;
+            txtCantidadE.Text = cantidad;
         }
 
         private void btnCancelarE_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (ValidateProduct.Set_ValidateProduct(txtNombreproductoE.Text, txtPrecioE.Text, txtCantidadE.Text)) //METODO PARA VALIDAR LOS VALORES INTRODUCIDOS POR EL USUARIO
+            {
+                //Si los valores son del tipo correcto
+                if (db.UpdateProduct(id, txtNombreproductoE.Text, txtPrecioE.Text, txtCantidadE.Text)) //Introduce el producto a la base de datos
+                {
+                    //Si se inserto el porducto correctamente
+                    MessageBox.Show("Se actualizo el producto ocn exito");
+                    Cleartxt();
+                    frm.loadData();
+                }
+                else
+                {
+                    //Si ocurre un error
+                    MessageBox.Show("Ocurrio un error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserte los valores correctos");
+            }
+        }
+
+        private void Cleartxt()
+        {
+            txtNombreproductoE.Text = "";
+            txtPrecioE.Text = "";
+            txtCantidadE.Text = "";
         }
     }
 }
